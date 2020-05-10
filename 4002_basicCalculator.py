8f7ea00e1caddb42b4a01a8c0d9ca6f5083cb575
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-05-10 10:40:20 +0800

    Create 4002_basicCalculator.py

---
+++ 4002_basicCalculator.py
@@ -0,0 +1,25 @@
+
+'''
+This is a simple calculator with two functions. 
+There are three inputs from the user, two operands and one operator
+'''
+
+def addition(x,y):
+    add = 0
+    addVal = int(x) + int(y)
+    return addVal
+
+def subtraction(x,y):
+    sub = 0
+    subVal = int(x) - int(y)
+    return subVal
+
+numOne = input("Input a first number: ")
+numTwo = input("Input a second number: ")
+funcChoose = input("Input a function: (add/sub) ")
+
+if funcChoose == "add":
+    print(addition(numOne, numTwo))
+
+else:
+    print(subtraction(numOne, numTwo))
